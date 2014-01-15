5248e2ff739e9bca55dd44d1c2ae2565ff8d7d30
Jeremy Clerc <jeremy@clerc.io>
2014-01-15 21:42:26 +0100

    first commit, version is working

---
+++ README.md
@@ -0,0 +1,25 @@
+This is a simple script that takes users information from a csv file and 
+creates them in GitLab
+
+Tested with GitLab 6.4.3 API v3
+
+Requires
+========
+* Python
+     easy\_install requests termcolor 
+
+Usage
+=====
+usage: create_gitlab_users.py [-h] --token TOKEN --url URL users.csv
+
+Create gitlab users from csv file, with format:
+Name;Username;email;Group1,Group2,Groupn
+
+positional arguments:
+  users.csv             A csv file containing users.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --token TOKEN, -t TOKEN
+                        User api token
+  --url URL, -u URL     API Url: http://gitlab.example.com/api/v3
---
+++ create_gitlab_users.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+"""
+  run --help for informations
+
+  easy_install requests termcolor 
+
+  Tested with GitLab 6.4.3 API v3 
+"""
+
+import os
+import sys
+import json
+import requests
+import csv
+import argparse
+from termcolor import colored, cprint
+
+
+parser = argparse.ArgumentParser(description='Create gitlab users from csv file, with format: Name;Username;email;Group1,Group2,Groupn')
+parser.add_argument('csv', metavar='users.csv', 
+                   help="A csv file containing users.\n")
+parser.add_argument('--token', '-t', required=True, 
+                   help='User api token')
+parser.add_argument('--url', '-u', required=True, 
+                   help='API Url: http://gitlab.example.com/api/v3')
+args = parser.parse_args()
+
+apiurl = args.url
+headers = {'content-type': 'application/json', 'PRIVATE-TOKEN': args.token}
+# dummy password, users will need to do password reset
+password = "WoalRoocReysIcCa"
+groups = {}
+
+def add_user_to_group(userid, groupid):
+   # access_level = 30 -> Developer
+    payload = {"id": groupid, "user_id": userid, "access_level": 30}
+    r = requests.post(apiurl + '/groups/' + str(groupid) + '/members', data=json.dumps(payload), headers=headers)
+
+    if r.status_code == 201:
+      return True
+    else:
+      return False
+
+# getting all groups, so we can convert name to id
+r = requests.get(apiurl + '/groups/', headers=headers)
+if r.status_code == 200:
+  jsobj = json.loads(r.text)
+  for group in jsobj:
+    if group.has_key('id') and group.has_key('name'):
+      groups[group['name']] = group['id']
+else:
+  sys.stderr.write(colored("Getting groups FAILED.", "red", attrs['bold']))
+  sys.exit(1)
+
+with open(args.csv, 'r') as csvfile:
+  usersreader = csv.reader(csvfile, delimiter=';')
+  for user in usersreader:
+    name = user[0]
+    username = user[1]
+    email = user[2]
+    usergroups = user[3].split(",")
+    createusrmsg = colored("Creating user [" + name + "] with username [" + username + "] and email [" + email + "]: ", "cyan")
+    payload = {"username": username, "email": email, "name": name,"password": password}
+    r = requests.post(apiurl + '/users/', data=json.dumps(payload), headers=headers)
+
+    if r.status_code == 201:
+      print createusrmsg + colored("OK", "green")
+      jsobj = json.loads(r.text)
+      if jsobj.has_key('id'):
+        # add user to groups
+        for group in usergroups:
+          creategrpmsg = colored("\tAdding user [" + name + "] to group [" + group + "]: ", "cyan")
+          if add_user_to_group(jsobj['id'], groups[group]):
+            print creategrpmsg + colored("OK", "green")
+          else:
+            print creategrpmsg + colored("FAILED", "red", attrs=['bold'])
+    elif r.status_code == 404:
+      print createusrmsg + colored("FAILED", "red", attrs=['bold'])
+    else:
+      print createusrmsg + colored("UNKNOWN ERROR", "red", attrs=['bold'])
